4c1d6e1c4e6bd20b8b14504de3e90e9489ad4b0b
Javi <nhitbh@gmail.com>
2018-08-06 22:08:08 +0200

    Enable auto popup when typeChar is '.'

--- src/main/java/com/fooock/solidity/lang/completion/SolidityCompletion.java
+++ src/main/java/com/fooock/solidity/lang/completion/SolidityCompletion.java
@@ -1,10 +1,17 @@
 package com.fooock.solidity.lang.completion;
 
 import com.intellij.codeInsight.completion.CompletionContributor;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Solidity code completion
  */
 public class SolidityCompletion extends CompletionContributor {
 
+    @Override
+    public boolean invokeAutoPopup(@NotNull PsiElement position, char typeChar) {
+        // show popup automatically when access member
+        return typeChar == '.';
+    }
 }
