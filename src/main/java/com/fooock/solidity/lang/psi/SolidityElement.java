88a3b9569aa12cf316cc170bd13b69820fcd34e1
Javi <nhitbh@gmail.com>
2018-08-15 16:43:46 +0200

    Make SolidityElement class implement NavigatablePsiElement

--- src/main/java/com/fooock/solidity/lang/psi/SolidityElement.java
+++ src/main/java/com/fooock/solidity/lang/psi/SolidityElement.java
@@ -1,9 +1,9 @@
 package com.fooock.solidity.lang.psi;
 
-import com.intellij.psi.PsiElement;
+import com.intellij.psi.NavigatablePsiElement;
 
 /**
  * Base interface for all PSI elements of the Solidity lang
  */
-public interface SolidityElement extends PsiElement {
+public interface SolidityElement extends NavigatablePsiElement {
 }
